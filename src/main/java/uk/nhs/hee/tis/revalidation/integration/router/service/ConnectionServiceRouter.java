2a597022ecdc517d14115103abd0b643fdf957ca
Doris Wong <doris.wong@hee.nhs.uk>
2021-02-26 17:06:03 +0000

    feat: route exception summary to new exception ES endpoint
    TIS21-1168

--- src/main/java/uk/nhs/hee/tis/revalidation/integration/router/service/ConnectionServiceRouter.java
+++ src/main/java/uk/nhs/hee/tis/revalidation/integration/router/service/ConnectionServiceRouter.java
@@ -52,6 +52,7 @@ public class ConnectionServiceRouter extends RouteBuilder {
   private static final String API_CONNECTION_HIDE = "/api/connections/hide?bridgeEndpoint=true";
   private static final String API_CONNECTION_UNHIDE = "/api/connections/unhide?bridgeEndpoint=true";
   private static final String API_CONNECTION_HIDDEN = "/api/connections/hidden?bridgeEndpoint=true";
+  private static final String API_CONNECTION_EXCEPTION = "/api/connections/exception?bridgeEndpoint=true";
   private static final String API_CONNECTION_TCS_HIDDEN = "/api/revalidation/connection/hidden/${header.gmcIds}?searchQuery=${header.searchQuery}&pageNumber=${header.pageNumber}&bridgeEndpoint=true";
   private static final String API_CONNECTION_TCS_EXCEPTION = "/api/revalidation/connection/exception/${header.gmcIds}?searchQuery=${header.searchQuery}&pageNumber=${header.pageNumber}&dbcs=${header.dbcs}&bridgeEndpoint=true";
   private static final String API_CONNECTION_DOCTOR_UNHIDDEN = "/api/v1/doctors/unhidden/${header.gmcIds}?bridgeEndpoint=true";
@@ -112,16 +113,7 @@ public class ConnectionServiceRouter extends RouteBuilder {
 
     // Connection summary page - Exceptions queue tab
     from("direct:connection-exception-summary")
-        .to("direct:connection-exception")
-        .setHeader("gmcIds").method(gmcIdProcessorBean, "getConnectionExceptionGmcIds")
-        .to("direct:v1-doctors-by-ids")
-        .enrich("direct:tcs-connection-exception", connectionExceptionAggregationStrategy);
-    from("direct:connection-exception")
-        .to(serviceUrlConnection + CONNECTION_EXCEPTION_API)
-        .unmarshal().json(JsonLibrary.Jackson);
-    from("direct:tcs-connection-exception")
-        .setHeader(OIDC_ACCESS_TOKEN_HEADER).method(keycloakBean, GET_TOKEN_METHOD)
-        .toD(tcsServiceUrl + API_CONNECTION_TCS_EXCEPTION)
+        .to(serviceUrlConnection + API_CONNECTION_EXCEPTION)
         .unmarshal().json(JsonLibrary.Jackson);
 
     // Connection summary page - Hidden tab
