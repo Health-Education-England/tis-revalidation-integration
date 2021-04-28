c627f99d06bc0aa0fb473430808a85717b13219c
tisadmincw <tisadmincw@N2810.xhee.nhs.uk>
2021-04-28 07:00:25 +0100

    wip: changed rabbit configuration strategy

--- src/main/java/uk/nhs/hee/tis/revalidation/integration/router/service/SyncServiceRouter.java
+++ src/main/java/uk/nhs/hee/tis/revalidation/integration/router/service/SyncServiceRouter.java
@@ -1,19 +1,14 @@
 package uk.nhs.hee.tis.revalidation.integration.router.service;
 
 import org.apache.camel.builder.RouteBuilder;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import uk.nhs.hee.tis.revalidation.integration.router.message.RabbitConfiguration;
 
 @Component
 public class SyncServiceRouter extends RouteBuilder {
 
-  @Autowired
-  RabbitConfiguration rabbitConfiguration;
-
   @Override
   public void configure() {
     from("direct:start-tis-sync")
-      .to(rabbitConfiguration.getSyncStartRoute());
+      .to("bean:syncStartHandler");
   }
 }
