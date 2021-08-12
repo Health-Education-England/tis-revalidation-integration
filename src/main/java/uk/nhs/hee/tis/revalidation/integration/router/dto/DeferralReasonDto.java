1e12bdf20b6b01b3f76d8dc456a5c004c55aa0a3
jayanta2018 <jayanta.saha@hee.nhs.uk>
2021-08-12 16:07:20 +0100

    feat: add status field to deferral reason dto
    TIS21-1960

--- src/main/java/uk/nhs/hee/tis/revalidation/integration/router/dto/DeferralReasonDto.java
+++ src/main/java/uk/nhs/hee/tis/revalidation/integration/router/dto/DeferralReasonDto.java
@@ -3,6 +3,7 @@ package uk.nhs.hee.tis.revalidation.integration.router.dto;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import java.util.List;
 import lombok.Data;
+import uk.nhs.hee.tis.revalidation.integration.enums.Status;
 
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
@@ -10,5 +11,7 @@ public class DeferralReasonDto {
 
   private String code;
   private String reason;
+  private String abbr;
   private List<DeferralReasonDto> subReasons;
+  private Status status;
 }
