184cf6ab524ccdc945196173799955fdded16809
TISadminCW <TISadminCW@N3647.xhee.nhs.uk>
2022-04-14 11:09:18 +0100

    chore: remove unused constants

--- src/main/java/uk/nhs/hee/tis/revalidation/integration/cdc/DoctorConstants.java
+++ src/main/java/uk/nhs/hee/tis/revalidation/integration/cdc/DoctorConstants.java
@@ -25,13 +25,10 @@ import lombok.experimental.UtilityClass;
 
 @UtilityClass
 public class DoctorConstants {
-  public static final String GMC_REFERENCE_NUMBER = "gmcReferenceNumber";
   public static final String DOCTOR_FIRST_NAME = "doctorFirstName";
   public static final String DOCTOR_LAST_NAME = "doctorLastName";
   public static final String SUBMISSION_DATE = "submissionDate";
-  public static final String DATE_ADDED = "dateAdded";
   public static final String UNDER_NOTICE = "underNotice";
-  public static final String SANCTION = "sanction";
   public static final String DOCTOR_STATUS = "doctorStatus";
   public static final String LAST_UPDATED_DATE = "lastUpdatedDate";
   public static final String DESIGNATED_BODY_CODE = "designatedBodyCode";
--- src/main/java/uk/nhs/hee/tis/revalidation/integration/cdc/RecommendationConstants.java
+++ src/main/java/uk/nhs/hee/tis/revalidation/integration/cdc/RecommendationConstants.java
@@ -25,17 +25,5 @@ import lombok.experimental.UtilityClass;
 
 @UtilityClass
 public class RecommendationConstants {
-  public static final String RECOMMENDATION_ID = "id";
-  public static final String GMC_NUMBER = "gmcNumber";
   public static final String OUTCOME = "outcome";
-  public static final String RECOMMENDATION_TYPE = "recommendationType";
-  public static final String RECOMMENDATION_STATUS = "recommendationStatus";
-  public static final String GMC_SUBMISSION_DATE = "gmcSubmissionDate";
-  public static final String ACTUAL_SUBMISSION_DATE = "actualSubmissionDate";
-  public static final String GMC_REVALIDATION_ID = "gmcRevalidationId";
-  public static final String DEFERRAL_DATE = "deferralDate";
-  public static final String DEFERRAL_REASON = "deferralReason";
-  public static final String DEFERRAL_SUB_REASON = "deferralSubReason";
-  public static final String COMMENTS = "comments";
-  public static final String RECOMMENDATION_ADMIN = "admin";
 }
