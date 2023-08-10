7a272875535f44916cdc1d39cb226b1ee031f3b5
Cai Willis <Cai.Willis@hee.nhs.uk>
2023-08-10 09:59:47 +0100

    chore: code smells
    TIS21-4459

--- src/main/java/uk/nhs/hee/tis/revalidation/integration/cdc/message/handler/CdcTraineeUpdateMessageHandler.java
+++ src/main/java/uk/nhs/hee/tis/revalidation/integration/cdc/message/handler/CdcTraineeUpdateMessageHandler.java
@@ -39,7 +39,7 @@ public class CdcTraineeUpdateMessageHandler {
   }
 
   public void handleMessage(ConnectionInfoDto message) {
-      cdcTraineeUpdateService.upsertEntity(message);
+    cdcTraineeUpdateService.upsertEntity(message);
   }
 
 }
--- src/main/java/uk/nhs/hee/tis/revalidation/integration/router/mapper/TraineeConnectionMapper.java
+++ src/main/java/uk/nhs/hee/tis/revalidation/integration/router/mapper/TraineeConnectionMapper.java
@@ -35,7 +35,7 @@ public interface TraineeConnectionMapper {
   @Mapping(target = "tcsPersonId", ignore = true)
   @Mapping(target = "curriculumEndDate", ignore = true)
   @Mapping(target = "syncEnd", ignore = true)
-  @Mapping(target= "placementGrade", ignore = true)
+  @Mapping(target = "placementGrade", ignore = true)
   ConnectionInfoDto mergeTraineeConnectionResponses(TraineeInfoDto traineeInfoDto,
       ConnectionRecordDto connectionRecordDto);
 }
