560948e2ec219c0c3e63de1ca6d3599a37f4bf5d
jayanta2018 <jayanta.saha@hee.nhs.uk>
2025-04-17 14:30:16 +0100

    fix: fix discrepancies filter string

--- src/main/java/uk/nhs/hee/tis/revalidation/integration/sync/service/DoctorUpsertElasticSearchService.java
+++ src/main/java/uk/nhs/hee/tis/revalidation/integration/sync/service/DoctorUpsertElasticSearchService.java
@@ -40,12 +40,15 @@ public class DoctorUpsertElasticSearchService {
 
   protected static final String ES_CURRENT_CONNECIONS_FILTER = "{\"term\":{\"existsInGmc\":true}}";
   protected static final String ES_DISCREPANCIES_FILTER =
-      "{\"filter\":{"
-         + "\"script\":{"
-         + "\"script\":\"doc['tcsDesignatedBody.keyword'] != doc['designatedBody.keyword']\""
-         + "}"
-         + "}}";
-
+      """
+         {
+           "filter": {
+              "script": {
+                "script": "doc['tcsDesignatedBody.keyword'] != doc['designatedBody.keyword']"
+              }
+            }
+         }
+       """;
   protected static final String ES_INDEX = "masterdoctorindex";
   protected static final String DISCREPANCIES_ALIAS = "discrepancies";
   protected static final String CURRENT_CONNECTIONS_ALIAS = "current_connections";
