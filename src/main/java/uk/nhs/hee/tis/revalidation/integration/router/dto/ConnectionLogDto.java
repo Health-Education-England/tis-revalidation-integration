c24deacfced9050856fc71e39d974e5cad5a77b7
Yafang Deng <hihilary@outlook.com>
2025-12-11 09:34:42 +0000

    fix: code smell
    TIS21-7880

--- src/main/java/uk/nhs/hee/tis/revalidation/integration/router/dto/ConnectionLogDto.java
+++ src/main/java/uk/nhs/hee/tis/revalidation/integration/router/dto/ConnectionLogDto.java
@@ -6,6 +6,9 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+/**
+ * A data class to handle connection log details.
+ */
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
